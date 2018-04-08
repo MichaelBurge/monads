dce2b4b21fc578184845c4ca9fb736edd734579c
Michael Burge <michaelburge@pobox.com>
2018-04-08 04:40:12 -0700

    Add Esa Pulkkinen's suggestions

--- monads.js
+++ monads.js
@@ -15,7 +15,11 @@ var metaphors = [
     "monads",
     "a monoid with smart pipes",
     "composable functions with external semantics",
-    "programmable semicolons"
+    "programmable semicolons",
+    "factories for sequentially composed commands",
+    "triples",
+    "comonad duals",
+    "automated workflows"
     ];
 
 var discoverNewMetaphor = function() {
